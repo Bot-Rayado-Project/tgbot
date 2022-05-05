a0d9009aff9de2f53a72f1100ab669a55acafc95
o3xiced <3xiced@gmail.com>
2022-05-06 00:20:30 +0300

    fix 2(❁´◡❁)

--- botrayado/database/db.py
+++ botrayado/database/db.py
@@ -12,6 +12,7 @@ logger = get_logger(__name__)
 
 sqlite_connection = psycopg2.connect(
     dbname=DBNAME, user=DBUSER, password=DBPASSWORD, host=DBHOST)
+cursor = sqlite_connection.cursor()
 
 
 def database_handler(ret_cfg: bool = False):
@@ -19,61 +20,55 @@ def database_handler(ret_cfg: bool = False):
         async def wrapper(msg: types.Message) -> str:
 
             logger.info(f'Request: {msg.from_user.username} - {msg.text}')
-            with sqlite_connection:
-                with sqlite_connection.cursor() as cursor:
-                    cursor.execute(ADD_COMMAND.format(
-                        msg.from_user.id, msg.text))
+            cursor.execute(ADD_COMMAND.format(
+                msg.from_user.id, msg.text))
+            sqlite_connection.commit()
+            if ret_cfg:
+                cursor.execute(
+                    SELECT_CONFIG_KEYBOARD_BUTTONS.format(msg.from_user.id))
+                btn = cursor.fetchall()
+                if btn == []:
+                    cursor.execute(
+                        FIRST_ADD_CONFIG_BUTTONS.format(msg.from_user.id))
                     sqlite_connection.commit()
-                    if ret_cfg:
-                        cursor.execute(
-                            SELECT_CONFIG_KEYBOARD_BUTTONS.format(msg.from_user.id))
-                        btn = cursor.fetchall()
-                        if btn == []:
-                            cursor.execute(
-                                FIRST_ADD_CONFIG_BUTTONS.format(msg.from_user.id))
-                            sqlite_connection.commit()
-                            # Вернет кнопки клавы с пустыми ячейками
-                            return await func(msg, btn)
-                        else:
-                            cursor.execute(
-                                SELECT_CONFIG_KEYBOARD_BUTTONS.format(msg.from_user.id))
-                            # Вернет кнопки клавы с чем то уже имеющимся
-                            return await func(msg, cursor.fetchall())
-                    else:
-                        return await func(msg)
+                    # Вернет кнопки клавы с пустыми ячейками
+                    return await func(msg, btn)
+                else:
+                    cursor.execute(
+                        SELECT_CONFIG_KEYBOARD_BUTTONS.format(msg.from_user.id))
+                    # Вернет кнопки клавы с чем то уже имеющимся
+                    return await func(msg, cursor.fetchall())
+            else:
+                return await func(msg)
 
         return wrapper
     return decorator
 
 
 def set_button_blueprint(cmd: str, msg: types.Message, sc_btn: str) -> bool:
-    with sqlite_connection:
-        with sqlite_connection.cursor() as cursor:
 
-            cursor.execute(
-                SELECT_CONFIG_KEYBOARD_BUTTONS.format(msg.from_user.id))
-            btn = cursor.fetchall()[0][0].split(', ')
+    cursor.execute(
+        SELECT_CONFIG_KEYBOARD_BUTTONS.format(msg.from_user.id))
+    btn = cursor.fetchall()[0][0].split(', ')
 
-            if sc_btn == btn[0]:
-                cursor.execute(UPDATE_CONFIG_BUTTONS.format(
-                    cmd, btn[1], btn[2], msg.from_user.id))
+    if sc_btn == btn[0]:
+        cursor.execute(UPDATE_CONFIG_BUTTONS.format(
+            cmd, btn[1], btn[2], msg.from_user.id))
 
-            elif sc_btn == btn[1]:
-                cursor.execute(UPDATE_CONFIG_BUTTONS.format(
-                    btn[0], cmd, btn[2], msg.from_user.id))
+    elif sc_btn == btn[1]:
+        cursor.execute(UPDATE_CONFIG_BUTTONS.format(
+            btn[0], cmd, btn[2], msg.from_user.id))
 
-            elif sc_btn == btn[2]:
-                cursor.execute(UPDATE_CONFIG_BUTTONS.format(
-                    btn[0], btn[1], cmd, msg.from_user.id))
+    elif sc_btn == btn[2]:
+        cursor.execute(UPDATE_CONFIG_BUTTONS.format(
+            btn[0], btn[1], cmd, msg.from_user.id))
 
-            sqlite_connection.commit()
+    sqlite_connection.commit()
 
 
 async def fetch_commands(msg: types.Message) -> list:
 
-    with sqlite_connection:
-        with sqlite_connection.cursor() as cursor:
-            cursor.execute(SELLECT_ALL_COMMANDS.format(msg.from_user.id))
-            cmd = cursor.fetchall()
+    cursor.execute(SELLECT_ALL_COMMANDS.format(msg.from_user.id))
+    cmd = cursor.fetchall()
 
-            return cmd
+    return cmd
